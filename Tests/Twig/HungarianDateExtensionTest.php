0369890631640c554f88887df6fb53d2fc063d45
Peter Simon <simonpepe@gmail.com>
2013-05-22 11:37:09 +0200

    Refactoring tests

--- Tests/Twig/HungarianDateExtensionTest.php
+++ Tests/Twig/HungarianDateExtensionTest.php
@@ -13,131 +13,163 @@ class HungarianDateExtensionTest extends \PHPUnit_Framework_TestCase
         $this->hde = new HungarianDateExtension();
     }
 
+    protected function checkToHungarianDate($out, $date)
+    {
+        $this->assertEquals($out, $this->hde->toHungarianDate($date));
+    }
+
+    protected function checkToHungarianDateWithHtml($out, $date)
+    {
+        $this->assertEquals($out, $this->hde->toHungarianDateWithHtml($date));
+    }
+
+    protected function checkToHungarianDateTime($out, $date)
+    {
+        $this->assertEquals($out, $this->hde->toHungarianDateTime($date));
+    }
+
+    protected function checkToHungarianDateTimeWithHtml($out, $date)
+    {
+        $this->assertEquals($out, $this->hde->toHungarianDateTimeWithHtml($date));
+    }
+
+
     public function testToHungarianDate()
     {
         // Ekezetes karakterek kezelese
-        $this->assertEquals(
-            '2012. január 12.',
-            $this->hde->toHungarianDate(new \DateTime('2012-01-12'))
+        $this->checkToHungarianDate(
+            '2012. január 12.', new \DateTime('2012-01-12')
         );
 
         // Felesleges szokozok ellenorzese
-        $this->assertEquals(
-            '2012. december 2.',
-            $this->hde->toHungarianDate(new \DateTime('2012-12-02'))
+        $this->checkToHungarianDate(
+            '2012. december 2.', new \DateTime('2012-12-02')
         );
 
         // unix timestamp elfogadasa
-        $this->assertEquals(
-            '2012. december 2.',
-            $this->hde->toHungarianDate(strtotime('2012-12-02'))
+        $this->checkToHungarianDate(
+            '2012. december 2.', new \DateTime('2012-12-02')
         );
 
-        $this->assertEquals(
-            '2012. december 2.',
-            $this->hde->toHungarianDate((string)strtotime('2012-12-02'))
+        $this->checkToHungarianDate(
+            '2012. december 2.', (string)strtotime('2012-12-02')
         );
 
         // string-ben megadott datum elfogadasa
-        $this->assertEquals(
-            '2012. december 2.',
-            $this->hde->toHungarianDate('2012-12-02')
+        $this->checkToHungarianDate(
+            '2012. december 2.', '2012-12-02'
         );
     }
 
     public function testToHungarianDateWithHtml()
     {
         // Ekezetes karakterek kezelese
-        $this->assertEquals(
-            '<time title="2012. január 12. csütörtök 15:16">2012. január 12.</time>',
-            $this->hde->toHungarianDateWithHtml(new \DateTime('2012-01-12 15:16'))
+        $this->checkToHungarianDateWithHtml(
+            '<time title="2012. január 12. csütörtök 15:16">' .
+                '2012. január 12.' .
+            '</time>',
+            new \DateTime('2012-01-12 15:16')
         );
 
         // Felesleges szokozok ellenorzese
-        $this->assertEquals(
-            '<time title="2012. december 2. vasárnap 15:16">2012. december 2.</time>',
-            $this->hde->toHungarianDateWithHtml(new \DateTime('2012-12-02 15:16'))
+        $this->checkToHungarianDateWithHtml(
+            '<time title="2012. december 2. vasárnap 15:16">' .
+                '2012. december 2.' .
+            '</time>',
+            new \DateTime('2012-12-02 15:16')
         );
 
         // unix timestamp elfogadasa
-        $this->assertEquals(
-            '<time title="2012. december 2. vasárnap 15:16">2012. december 2.</time>',
-            $this->hde->toHungarianDateWithHtml(strtotime('2012-12-02 15:16'))
+        $this->checkToHungarianDateWithHtml(
+            '<time title="2012. december 2. vasárnap 15:16">' .
+                '2012. december 2.' .
+            '</time>',
+            strtotime('2012-12-02 15:16')
         );
 
-        $this->assertEquals(
-            '<time title="2012. december 2. vasárnap 15:16">2012. december 2.</time>',
-            $this->hde->toHungarianDateWithHtml((string)strtotime('2012-12-02 15:16'))
+        $this->checkToHungarianDateWithHtml(
+            '<time title="2012. december 2. vasárnap 15:16">' .
+                '2012. december 2.' .
+            '</time>',
+            (string)strtotime('2012-12-02 15:16')
         );
 
         // string-ben megadott datum elfogadasa
-        $this->assertEquals(
-            '<time title="2012. december 2. vasárnap 15:16">2012. december 2.</time>',
-            $this->hde->toHungarianDateWithHtml('2012-12-02 15:16')
+        $this->checkToHungarianDateWithHtml(
+            '<time title="2012. december 2. vasárnap 15:16">' .
+                '2012. december 2.' .
+            '</time>',
+            '2012-12-02 15:16'
         );
     }
 
     public function testToHungarianDateTime()
     {
         // Ekezetes karakterek kezelese
-        $this->assertEquals(
-            '2012. augusztus 13. hétfő 15:16',
-            $this->hde->toHungarianDateTime(new \DateTime('2012-08-13 15:16'))
+        $this->checkToHungarianDateTime(
+            '2012. augusztus 13. hétfő 15:16', new \DateTime('2012-08-13 15:16')
         );
 
         // Felesleges szokozok ellenorzese
-        $this->assertEquals(
-            '2012. augusztus 8. szerda 6:05',
-            $this->hde->toHungarianDateTime(new \DateTime('2012-08-08 06:05'))
+        $this->checkToHungarianDateTime(
+            '2012. augusztus 8. szerda 6:05', new \DateTime('2012-08-08 06:05')
         );
 
         // unix timestamp elfogadasa
-        $this->assertEquals(
-            '2012. december 2. vasárnap 15:16',
-            $this->hde->toHungarianDateTime(strtotime('2012-12-02 15:16'))
+        $this->checkToHungarianDateTime(
+            '2012. december 2. vasárnap 15:16', strtotime('2012-12-02 15:16')
         );
 
-        $this->assertEquals(
+        $this->checkToHungarianDateTime(
             '2012. december 2. vasárnap 15:16',
-            $this->hde->toHungarianDateTime((string)strtotime('2012-12-02 15:16'))
+            (string)strtotime('2012-12-02 15:16')
         );
 
         // string-ben megadott datum elfogadasa
-        $this->assertEquals(
-            '2012. december 2. vasárnap 15:16',
-            $this->hde->toHungarianDateTime('2012-12-02 15:16')
+        $this->checkToHungarianDateTime(
+            '2012. december 2. vasárnap 15:16', '2012-12-02 15:16'
         );
     }
 
     public function testToHungarianDateTimeWithHtml()
     {
         // Ekezetes karakterek kezelese
-        $this->assertEquals(
-            '<time title="2012. augusztus 13. hétfő 15:16">2012. augusztus 13. hétfő 15:16</time>',
-            $this->hde->toHungarianDateTimeWithHtml(new \DateTime('2012-08-13 15:16'))
+        $this->checkToHungarianDateTimeWithHtml(
+            '<time title="2012. augusztus 13. hétfő 15:16">' .
+                '2012. augusztus 13. hétfő 15:16' .
+            '</time>',
+            new \DateTime('2012-08-13 15:16')
         );
 
         // Felesleges szokozok ellenorzese
-        $this->assertEquals(
-            '<time title="2012. augusztus 8. szerda 6:05">2012. augusztus 8. szerda 6:05</time>',
-            $this->hde->toHungarianDateTimeWithHtml(new \DateTime('2012-08-08 06:05'))
+        $this->checkToHungarianDateTimeWithHtml(
+            '<time title="2012. augusztus 8. szerda 6:05">' .
+                '2012. augusztus 8. szerda 6:05' .
+            '</time>',
+            new \DateTime('2012-08-08 06:05')
         );
 
         // unix timestamp elfogadasa
-        $this->assertEquals(
-            '<time title="2012. december 2. vasárnap 15:16">2012. december 2. vasárnap 15:16</time>',
-            $this->hde->toHungarianDateTimeWithHtml(strtotime('2012-12-02 15:16'))
+        $this->checkToHungarianDateTimeWithHtml(
+            '<time title="2012. december 2. vasárnap 15:16">' .
+                '2012. december 2. vasárnap 15:16' .
+            '</time>',
+            strtotime('2012-12-02 15:16')
         );
 
-        $this->assertEquals(
-            '<time title="2012. december 2. vasárnap 15:16">2012. december 2. vasárnap 15:16</time>',
-            $this->hde->toHungarianDateTimeWithHtml((string)strtotime('2012-12-02 15:16'))
+        $this->checkToHungarianDateTimeWithHtml(
+            '<time title="2012. december 2. vasárnap 15:16">' .
+                '2012. december 2. vasárnap 15:16' .
+            '</time>',
+            (string)strtotime('2012-12-02 15:16')
         );
 
         // string-ben megadott datum elfogadasa
-        $this->assertEquals(
-            '<time title="2012. december 2. vasárnap 15:16">2012. december 2. vasárnap 15:16</time>',
-            $this->hde->toHungarianDateTimeWithHtml('2012-12-02 15:16')
+        $this->checkToHungarianDateTimeWithHtml(
+            '<time title="2012. december 2. vasárnap 15:16">' .
+                '2012. december 2. vasárnap 15:16' .
+            '</time>',
+            '2012-12-02 15:16'
         );
     }
 }
