88a8a0710e0a5d8eb1cc8d08b094c91ac227b676
Peter Simon <simonpepe@gmail.com>
2015-12-16 15:21:22 +0100

    Fix Twig depreciation for Symfony 2.8/3.0

--- Twig/HungarianDateExtension.php
+++ Twig/HungarianDateExtension.php
@@ -25,17 +25,21 @@ class HungarianDateExtension extends \Twig_Extension
     public function getFilters()
     {
         return array(
-            'hungarian_date' => new \Twig_Filter_Method(
-                $this, 'toHungarianDate'
+            new \Twig_SimpleFilter(
+                'hungarian_date', array($this, 'toHungarianDate')
             ),
-            'hungarian_date_tag' => new \Twig_Filter_Method(
-                $this, 'toHungarianDateWithHtml', array('is_safe' => array('html'))
+            new \Twig_SimpleFilter(
+                'hungarian_date_tag',
+                array($this, 'toHungarianDateWithHtml'),
+                array('is_safe' => array('html'))
             ),
-            'hungarian_datetime' => new \Twig_Filter_Method(
-                $this, 'toHungarianDateTime'
+            new \Twig_SimpleFilter(
+                'hungarian_datetime', array($this, 'toHungarianDateTime')
             ),
-            'hungarian_datetime_tag' => new \Twig_Filter_Method(
-                $this, 'toHungarianDateTimeWithHtml', array('is_safe' => array('html'))
+            new \Twig_SimpleFilter(
+                'hungarian_datetime_tag',
+                array($this, 'toHungarianDateTimeWithHtml'),
+                array('is_safe' => array('html'))
             ),
         );
     }
